8058c7a079f8ffd585689b99c0389ab8ba4bf191
Aritra Dutta <aritra.duttas18@gmail.com>
2025-07-19 22:56:53 +0530

    Added Animation to category

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,18 +5,41 @@ import 'package:meals_app/screens/meals_screen.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 import 'package:meals_app/data/dummy_data.dart';
 
-class CategoriesScreen extends StatelessWidget {
-
+class CategoriesScreen extends StatefulWidget {
   final List<MealBlueprint> availableFilteredMeals;
 
-  const CategoriesScreen({
-    super.key,
-    required this.availableFilteredMeals,
-  });
+  const CategoriesScreen({super.key, required this.availableFilteredMeals});
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerProviderStateMixin {
+  // Class variables.
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   void _selectCategory(BuildContext context, CategoryBlueprint category) {
-    final List<MealBlueprint> filteredCategoryMeals = 
-    availableFilteredMeals.where((eachMealItem) {
+    final List<MealBlueprint> filteredCategoryMeals = widget.availableFilteredMeals.where((eachMealItem) {
       return eachMealItem.categories.contains(category.id);
     }).toList();
 
@@ -24,10 +47,7 @@ class CategoriesScreen extends StatelessWidget {
       context,
       MaterialPageRoute(
         builder: (ctx) {
-          return MealsScreen(
-            title: category.title, 
-            meals: filteredCategoryMeals,
-          );
+          return MealsScreen(title: category.title, meals: filteredCategoryMeals);
         },
       ),
     );
@@ -35,27 +55,46 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(24),
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: const EdgeInsets.all(24),
 
-      // Grid Configurations
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+        // Grid Configurations
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+
+        // Grid Childs.
+        children: [
+          for (var eachCategory in availableCategories)
+            CategoryGridItem(
+              category: eachCategory,
+              onSelectCategory: () {
+                _selectCategory(context, eachCategory);
+              },
+            ),
+        ],
       ),
-
-      // Grid Childs.
-      children: [
-        for (var eachCategory in availableCategories)
-          CategoryGridItem(
-            category: eachCategory,
-            onSelectCategory: () {
-              _selectCategory(context, eachCategory);
-            },
-          ),
-      ],
+      builder: (context, child) {
+        return SlideTransition(
+          // In here child refers to the "GridView"
+          // Bcz, it has been passed as a child of the AnimatedBuilder.
+          position: Tween(
+              begin: Offset(0, 0.3),
+              end: Offset(0, 0)
+            ).animate(
+              CurvedAnimation(
+                parent: _animationController,
+                curve: Curves.easeInOut,
+              )
+            ),
+          child: child,
+        );
+      },
     );
   }
 }
